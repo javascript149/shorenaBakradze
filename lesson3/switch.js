294905f1a7fea89911224b0389c9440580c2f8e9
shorenabakradze <136688121+shorenabakradze@users.noreply.github.com>
2023-09-14 08:09:14 +0400

    finished homework3

---
+++ lesson3/switch.js
@@ -0,0 +1,36 @@
+let action = "move"
+switch(action) {
+    case "move":
+alert("You are moving!");
+    break;
+    case "jump":
+alert("you are jumping!");
+    break;
+    case "run":
+alert("You are running");
+    break;
+    default:
+alert("invalid action");
+    break;
+};
+
+switch (vegetables) {
+case "Carrot":
+alert ("Hello");
+break;
+case "Broccoli":
+alert ("Welcome");
+break;
+};
+
+
+switch (vegetables) {
+case "Broccoli":
+alert ("Hello");
+break;
+case "Carrot":
+alert ("Welcome");
+break;
+case "default":
+alert("Neither");
+};
